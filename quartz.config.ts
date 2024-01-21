11964ee82d267e4be0a7d292f10c7e7a5a359f49
jongunchae <cha452jg@gmail.com>
2024-01-21 17:06:31 +0900

    Quartz sync: Jan 21, 2024, 5:06 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "astrod.dev",
+    baseUrl: "astrod.pages.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
